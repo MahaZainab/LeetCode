eaf7a18f1338eb6c0e19b84ef0a0d69230401add
Maha Zainab <111684119+MahaZainab@users.noreply.github.com>
2024-07-24 23:21:10 +0500

    Time: 942 ms (80.98%), Space: 24.8 MB (47.83%) - LeetHub

---
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.py
@@ -0,0 +1,27 @@
+class Solution:
+    def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
+         store_pairs = []
+
+         for i in range(len(nums)):
+            mapped_value = 0
+            temp = nums[i]
+
+            # Start making changes from the units place.
+            place = 1
+            # If the value initially is 0, return mapping[0] and index.
+            if temp == 0:
+                store_pairs.append((mapping[0], i))
+                continue
+            # Repeat the process for units, tenths, hundredths.. places.
+            while temp != 0:
+                mapped_value = place * mapping[temp % 10] + mapped_value
+                place *= 10
+                temp //= 10
+            store_pairs.append((mapped_value, i))
+
+        # Sort the array in non-decreasing order by the first value (default).
+         store_pairs.sort()
+         answer = [nums[pair[1]] for pair in store_pairs]
+
+         return answer
+        
