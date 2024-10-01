102d3ff61d8d917eb984ee19e1b657802c86f5a0
Maha Zainab <111684119+MahaZainab@users.noreply.github.com>
2024-10-01 09:43:32 +0500

    Time: 531 ms (21.66%), Space: 30.3 MB (91.94%) - LeetHub

---
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canArrange(self, arr: List[int], k: int) -> bool:
+        # Custom comparator to sort based on mod values.
+        arr = sorted(arr, key=lambda x: (k + x % k) % k)
+
+        # Assign the pairs with modulo 0 first.
+        start = 0
+        end = len(arr) - 1
+        while start < end:
+            if arr[start] % k != 0:
+                break
+            if arr[start + 1] % k != 0:
+                return False
+            start = start + 2
+
+        # Now, pick one element from the beginning and one element from the
+        # end.
+        while start < end:
+            if (arr[start] + arr[end]) % k != 0:
+                return False
+            start += 1
+            end -= 1
+
+        return True
