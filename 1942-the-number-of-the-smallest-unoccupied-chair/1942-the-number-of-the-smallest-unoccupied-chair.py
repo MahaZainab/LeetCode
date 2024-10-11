73488d321272844e885cc1b5f47e2bdb532bf5a1
Maha Zainab <111684119+MahaZainab@users.noreply.github.com>
2024-10-11 09:28:35 +0500

    Time: 541 ms (91.89%), Space: 22.6 MB (93.99%) - LeetHub

---
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.py
@@ -0,0 +1,36 @@
+class Solution:
+    def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
+        target_arrival = times[targetFriend][0]
+        times = sorted(
+            [
+                (arrival, leave, index)
+                for index, (arrival, leave) in enumerate(times)
+            ]
+        )
+
+        next_chair = 0
+        available_chairs = []
+        leaving_queue = []
+
+        for time in times:
+            arrival, leave, index = time
+
+            # Free up chairs based on current time
+            while leaving_queue and leaving_queue[0][0] <= arrival:
+                _, chair = heapq.heappop(leaving_queue)
+                heapq.heappush(available_chairs, chair)
+
+            if available_chairs:
+                current_chair = heapq.heappop(available_chairs)
+            else:
+                current_chair = next_chair
+                next_chair += 1
+
+            # Push current leave time and chair
+            heapq.heappush(leaving_queue, (leave, current_chair))
+
+            # Check if it's the target friend
+            if index == targetFriend:
+                return current_chair
+
+        return 0
