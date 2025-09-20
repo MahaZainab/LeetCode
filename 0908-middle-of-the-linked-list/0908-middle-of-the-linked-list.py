9354b8ac8d65cf9e57c02b328057646eb1b622de
Maha Zainab <111684119+MahaZainab@users.noreply.github.com>
2025-09-20 16:17:59 -0500

    Time: 0 ms (100%), Space: 17.8 MB (48.36%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
@@ -1,19 +1,26 @@
-#Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# #Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
             return head
-        size=0
-        temp=head
-        while temp:
-            size+=1
-            temp=temp.next
-        middle=size//2
-        for i in range(middle):
-            head=head.next
-        return head
-        
+        slow=fast=head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+        return slow
+        # if not head or not head.next:
+        #     return head
+        # size=0
+        # temp=head
+        # while temp:
+        #     size+=1
+        #     temp=temp.next
+        # middle=size//2
+        # for i in range(middle):
+        #     head=head.next
+        # return head
+        # the time complexity would be O(n)
