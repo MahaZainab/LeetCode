0e5b34d689ba450a08375a40ad76bc060e2ab343
Maha Zainab <111684119+MahaZainab@users.noreply.github.com>
2025-09-20 23:17:14 -0500

    Time: 0 ms (100%), Space: 17.6 MB (84.69%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -1,25 +1,40 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        N = 0
-        cur = head
-        while cur:
-            N += 1
-            cur = cur.next
-        
-        removeIndex = N - n
-        if removeIndex == 0:
+        # First calculate the size
+        size=0
+        temp=head
+        while temp:
+            size += 1
+            temp=temp.next
+        if size==n:
             return head.next
+        deleteToBe=size-n
+        prev=head
+        dummy=prev
+        for i in range(deleteToBe-1):
+            prev=prev.next
+        prev.next=prev.next.next
+        return dummy
+        # N = 0
+        # cur = head
+        # while cur:
+        #     N += 1
+        #     cur = cur.next
         
-        cur = head
-        for i in range(N - 1):
-            if (i + 1) == removeIndex:
-                cur.next = cur.next.next
-                break
-            cur = cur.next
-        return head
+        # removeIndex = N - n
+        # if removeIndex == 0:
+        #     return head.next
+        
+        # cur = head
+        # for i in range(N - 1):
+        #     if (i + 1) == removeIndex:
+        #         cur.next = cur.next.next
+        #         break
+        #     cur = cur.next
+        # return head
