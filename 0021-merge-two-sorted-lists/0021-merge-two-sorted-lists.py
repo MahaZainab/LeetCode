360b4452b850db6c3b7e020d2ac57fdd268153fd
Maha Zainab <111684119+MahaZainab@users.noreply.github.com>
2025-10-26 21:14:28 -0500

    Time: 0 ms (100%), Space: 17.8 MB (61.47%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -3,7 +3,6 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         dummy = node = ListNode()
@@ -19,3 +18,5 @@ class Solution:
         node.next = list1 or list2
 
         return dummy.next
+
+        
