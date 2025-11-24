80b4d768aafda2f608f94a77c1b14de757b97404
Maha Zainab <111684119+MahaZainab@users.noreply.github.com>
2025-11-23 20:55:45 -0600

    Time: 15 ms (40.56%), Space: 18 MB (48.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,55 +1,20 @@
 class Solution:
+    def hashFunction(self, n):
+        return n-97
     def isAnagram(self, s: str, t: str) -> bool:
+        arr1=[0]*26
+        arr2=[0]*26
         if len(s) != len(t):
             return False
-        mappair={}
-        for c in s:
-            if c in mappair:
-                mappair[c] += 1
-            else:
-                mappair[c] = 1
-        for c in t:
-            if c in mappair:
-                mappair[c] -= 1
-                if mappair[c]<0:
-                    return False
-            else:
-                False
-        for val in mappair.values():
-            if val !=0:
+        for i in range(len(s)):
+            index1=self.hashFunction(ord(s[i]))
+            arr1[index1]+=1
+            index2=self.hashFunction(ord(t[i]))
+            arr2[index2]+=1
+        for i in range(26):
+            if arr1[i]!=arr2[i]:
                 return False
         return True
-
-        # mappair={}
-        # for i in range(len(s)):
-        #     if s[i] in mappair:
-        #         mappair[s[i]] +=1
-        #     else:
-        #         mappair[s[i]]=1
-        # for i in range(len(t)):
-        #     if t[i] in mappair:
-        #         mappair[t[i]] -=1
-        #         if mappair[t[i]]<0:
-        #             return False
-        #     else:
-        #         return False
-        # for val in mappair.values():
-        #     if val!=0:
-        #         return False
-        # return True
-
-        # dicts={}
-        # dictt={}
-        # for i in range(len(s)):
-        #     if s[i] in dicts:
-        #         dicts[s[i]] +=1
-        #     else:
-        #         dicts[s[i]]=1
-
-        # for i in range(len(t)):            
-        #     if t[i] in dictt:
-        #         dictt[t[i]] +=1
-        #     else:
-        #         dictt[t[i]]=1
-        # return dictt==dicts
+            
+        
         
