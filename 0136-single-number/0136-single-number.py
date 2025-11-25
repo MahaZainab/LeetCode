7af06af91ce028e6d24262e6f39b77f3efa85a5d
Maha Zainab <111684119+MahaZainab@users.noreply.github.com>
2025-11-25 10:26:04 -0600

    Time: 1 ms (74.37%), Space: 19.6 MB (76.9%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -2,6 +2,6 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         xor=0
         for num in nums:
-            xor ^=num
+            xor ^= num
         return xor
         
