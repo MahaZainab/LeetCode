397b69c653068643573c3043cede1356522d3df8
Maha Zainab <111684119+MahaZainab@users.noreply.github.com>
2025-12-14 19:34:49 -0600

    Time: 31 ms (93.12%), Space: 17.5 MB (88.89%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,19 +1,28 @@
 class Solution:
     def fib(self, n: int) -> int:
+        if n==0 or n==1:
+            return n
         dp=[0]*(n+1)
+        dp[0]=0
+        dp[1]=1
+        for i in range(2, n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]
 
-        def helper(n, dp)->int:
-            if n==0:
-                return 0
-            if n==1:
-                return 1
-            if dp[n] !=0:
-                return dp[n]
-            fib1=self.fib(n-1)
-            fib2=self.fib(n-2)
-            dp[n]=fib1+fib2
-            return dp[n]
-        return helper(n, dp)
+        # dp=[0]*(n+1)
+
+        # def helper(n, dp)->int:
+        #     if n==0:
+        #         return 0
+        #     if n==1:
+        #         return 1
+        #     if dp[n] !=0:
+        #         return dp[n]
+        #     fib1=self.fib(n-1)
+        #     fib2=self.fib(n-2)
+        #     dp[n]=fib1+fib2
+        #     return dp[n]
+        # return helper(n, dp)
 
 
        
